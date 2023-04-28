40931c988f850e4d74f40489531ec59c3f2b0cc6
atwanzire <tatwanzire@gmail.com>
2023-04-28 22:28:25 +0300

    gallery

--- src/components/home/content.jsx
+++ src/components/home/content.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HeartIcon, ChatBubbleBottomCenterIcon } from "@heroicons/react/24/solid"
+import { HeartIcon, ChatBubbleBottomCenterIcon, PlusCircleIcon } from "@heroicons/react/24/solid"
 import img from "../images/abstract.jpg"
 import img2 from "../images/african-woman.jpg"
 import img3 from "../images/african_art.jpg"
@@ -18,7 +18,125 @@ export default function Content() {
         <>
             <div>
                 <div className="text-center mx-5">
-                    <div className="grid"></div>
+                    <div className="grid grid-cols-3 gap-2">
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img2} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img3} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img4} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img5} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img6} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img7} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img8} alt="" />
+                                <div className="img-btns">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-7/12 mt-14 flex items-center">
+                            <div>
+                                <img className="rounded-2xl artify-img" src={img9} alt="" />
+                                <div className="img-btns ">
+                                    <div className="grid grid-cols-4 m-auto items-center h-6">
+                                        <div><img src={img3} className="icon w-12 h-12 m-auto rounded-full border bottom-2 border-white hover:bg-white hover:p-1 hover:rounded-xl hover:w-14 hover:h-14" alt="" /></div>
+                                        <div><HeartIcon className="icon w-7 h-7 m-auto text-white hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><ChatBubbleBottomCenterIcon className="icon w-7 h-7 m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                        <div><PlusCircleIcon className="icon w-7 h-7 font-bold m-auto text-white  hover:text-red-900 hover:bg-white hover:p-2 hover:rounded-xl hover:h-10 hover:w-10" /></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
